6d2b4306de92ba499f04b6dd7e14e15b208509bb
Paul Jomy <123456789+pauljomy@users.noreply.github.com>
2025-11-21 16:22:13 +0530

    working index.js file

---
+++ index.js
@@ -0,0 +1,28 @@
+const one = Number(document.getElementById("one").textContent);
+const two = Number(document.getElementById("two").textContent);
+const three = Number(document.getElementById("three").textContent);
+const four = Number(document.getElementById("four").textContent);
+const five = Number(document.getElementById("five").textContent);
+const six = Number(document.getElementById("six").textContent);
+const seven = Number(document.getElementById("seven").textContent);
+const eight = Number(document.getElementById("eight").textContent);
+const nine = Number(document.getElementById("nine").textContent);
+const zero = Number(document.getElementById("zero").textContent);
+const doubleZero = Number(document.getElementById("double-zero").textContent);
+const dot = document.getElementById("dot");
+const equal = document.getElementById("equal");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+const division = document.getElementById("division");
+const modulo = document.getElementById("modulo");
+const backspace = document.getElementById("backspace");
+const clearScreen = document.getElementById("ac");
+
+const answer = document.getElementById("final-answer");
+const question = document.getElementById("question");
+
+equal.addEventListener("click", () => {});
+
+function addDigits(num1, num2) {
+  return num1 * num2;
+}
